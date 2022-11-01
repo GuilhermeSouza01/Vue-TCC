3515c7d3a7c91ee84cc41f5a5db3a488abde03cb
Guilherme Souza <ghostgui60@gmail.com>
2022-11-01 16:20:05 -0300

    Finish app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrappButton">
+    <button @click="callComponent = true" class="generateButton">Gerar Tabela</button>
+  </div>
+  <div class="container" >
+    <TableData v-if="callComponent"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableData from './components/TableData.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TableData,
+  },
+  data() {
+    return {
+      callComponent: false,
+    }
+  },
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/styles.css";
 </style>
---
+++ src/components/TableData.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="containerTable">
+        <table>
+            <thead>
+                <tr class="table100-head">
+                    <th className="column1">Indice</th>
+                    <th className="column2">Nome</th>
+                    <th className="column3">Idade</th>
+                    <th className="column4">Número</th>
+                    <th className="column5">Saldo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in myData" :key="item">
+                    <td>{{item.index + 1}}</td>
+                    <td>{{item.name}}</td>
+                    <td>{{item.age}}</td>
+                    <td>{{item.phone}}</td>
+                    <td>{{item.balance}}</td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+import myData from "../db/json-tccfile.json";
+
+
+let count = 1;
+export default {
+    name: "TableData",
+    data() {
+        return {
+            myData,
+            count,
+        }
+    }
+}
+</script>
